195b1e9c472913f1b593ecf8a5629bf2a5c40f84
YouCorp-2 <ramachandran.191996@gmail.com>
2020-08-22 16:33:33 +0530

    react_app

---
+++ src/Router/index.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React,{Component} from 'react';
+
+import './App.scss';
+
+function Router () {
+  return (
+    
+     <div className='Container'>
+           <div className='Header'>
+             <div className='header_left_content'>
+               Engage
+             </div>
+             <div className='header_left_content1'>
+                <div className='Prcing_tag'>pricing</div> 
+                <div className='Request_demo' onClick={()=>{}}>
+                    Request Demo
+                </div>
+             </div>
+           </div>
+        
+     </div>
+  
+  )
+}
+
+export default Router;
